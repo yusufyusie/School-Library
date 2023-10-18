63308ae6cdd0796a161cee827e8b0d13c658fae1
yusufyusie <yesuf023@gmail.com>
2023-10-19 00:22:01 +0300

    Update changes and fix comment

--- person.rb
+++ person.rb
@@ -3,7 +3,7 @@ class Person
   attr_accessor :name, :age
 
   def initialize(name: 'Unknown', age: 0, parent_permission: true)
-    @id = SecureRandom.uuid
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
