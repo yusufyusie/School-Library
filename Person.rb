131e609348d483b47de8ac1985018c9327dd7043
yusufyusie <yesuf023@gmail.com>
2023-10-18 16:24:58 +0300

    Add person class file

---
+++ Person.rb
@@ -0,0 +1,30 @@
+class Person
+  attr_reader :id, :name, :age
+
+  def initialize(name = "Unknown", age = 0, parent_permission = true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def age=(new_age)
+    @age = new_age
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+
+  public
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+end
