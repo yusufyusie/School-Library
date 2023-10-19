df6ec9707eec96802761ec6927cb5b56e92161a3
yusufyusie <yesuf023@gmail.com>
2023-10-19 14:50:27 +0300

    Create the relationship between Person and Book using the intermediate class Rental

--- person.rb
+++ person.rb
@@ -1,6 +1,6 @@
 require_relative 'nameable'
 class Person < Nameable
-  attr_reader :id
+  attr_reader :id, :rentals
   attr_accessor :name, :age
 
   def initialize(name: 'Unknown', age: 0, parent_permission: true)
@@ -9,6 +9,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def correct_name
@@ -25,5 +26,9 @@ class Person < Nameable
 
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def add_rental(book, date)
+    Rental.new(date, book, self)
   end
 end
