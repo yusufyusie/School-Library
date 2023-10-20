214beff35c5f82d1b8ce2350f9ce3e4cd1dc5554
yusufyusie <yesuf023@gmail.com>
2023-10-20 14:01:49 +0300

    Update more descrivtion menu

--- main.rb
+++ main.rb
@@ -1,49 +1,47 @@
+require_relative './app'
+
+class Menu
+  def initialize
+    @app = App.new(self)
+    puts 'Welcome to the School Library App!'
+    show_menu
+  end
+
+  def show_menu
+    puts "\nPlease choose an option by entering the number:
+      1 - List all books
+      2 - List all people
+      3 - Create a person
+      4 - Create a book
+      5 - Create a rental
+      6 - List all rentals for a given person id
+      7 - Exit"
+    user_input = gets.chomp
+    select_option(user_input)
+  end
+
+  def select_option(user_input)
+    menu_options = {
+      '1' => :list_all_books,
+      '2' => :list_all_people,
+      '3' => :create_person,
+      '4' => :create_book,
+      '5' => :create_rental,
+      '6' => :list_all_rentals,
+      '7' => :exit
+    }
+    selected_option = menu_options[user_input]
+    if selected_option.nil?
+      puts 'Invalid option selected!'
+    else
+      @app.send(selected_option)
+    end
+    show_menu
+  end
+end
 
 def main
-  menu = {
-    "1" => "List all books",
-    "2" => "List all people",
-    "3" => "Create a person",
-    "4" => "Create a book",
-    "5" => "Create a rental",
-    "6" => "List all rentals for a given person ID",
-    "7" => "Quit"
-  }
-
-  puts "Welcome to the Book Rental App!"
-  puts "What would you like to do?"
-
-  menu.each do |key, value|
-    puts "#{key}: #{value}"
-  end
-
-  option = gets.chomp
-  while option != "7"
-    case option
-    when "1"
-      list_all_books
-    when "2"
-      list_all_people
-    when "3"
-      person_type = "teacher"
-      puts "What type of person would you like to create? (teacher/student)"
-      person_type = gets.chomp
-      create_person(person_type)
-    when "4"
-      create_book
-    when "5"
-      create_rental
-    when "6"
-      puts "Enter the person's ID:"
-      person_id = gets.chomp.to_i
-      list_all_rentals_for_person(person_id)
-    else
-      puts "Invalid option"
-    end
-
-    puts "What would you like to do next?"
-    option = gets.chomp
-  end
-
-  puts "Goodbye!"
+  Menu.new
 end
+
+main
